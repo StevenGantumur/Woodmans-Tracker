214469dae35a05a40130e0be616f94011d808e76
StevenGantumur <gantumursteven@gmail.com>
2025-09-18 21:02:21 -0400

    Starting CorralGrid

--- frontend/src/Components/CorralGrid.jsx
+++ frontend/src/Components/CorralGrid.jsx
@@ -4,7 +4,9 @@ import React from "react";
 // Use this to style the grids
 import "./CorralGrid.css";
 
-
 function CorralGrid({ corrals }){
-    
+
+    const rows = ["A", "B", "C"];
+    const cols = Array.from({ length: 8 }, (_, i) => i+1);
+
 }
