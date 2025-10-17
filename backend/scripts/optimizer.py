3e54b5c7402ba22e7048d2f34c2b72b887e6985a
StevenGantumur <gantumursteven@gmail.com>
2025-10-16 21:02:59 -0400

    Update optimizer.py

--- backend/scripts/optimizer.py
+++ backend/scripts/optimizer.py
@@ -1,23 +1,15 @@
-# optimizer.py
-# -----------------
-# This module handles route optimization logic for the Woodman's cart collection project.
-# It uses Google OR-Tools to compute efficient paths for cart retrievers to move between
-# cart corrals and the building, minimizing total distance or travel time.
-#
-# The data is usually received from the backend REST API as JSON — 
-# containing corral coordinates and optionally the collector’s current position.
+import sys, json, math
+from ortools.constraint_solver import pywrapcp, routing_enumb_pb2
 
-from ortools.constraint_solver import pywrapcp, routing_enums_pb2
-import math
+# Helper Functions vvv
 
-
-# Helper Methods vvv
 def euclidean_distance(positionOne, positionTwo):
-    #Computes the straight line distance between the two positions (x1, y1) and (x2, y2).
-    return math.hypot(positionOne[0] - positionTwo[0], positionOne[1], positionTwo[1])
+    #Compute straight line distance between points (x1, y1) and (x2, y2).
+    return math.hypot(positionOne[0] - positionTwo[0], positionOne[1] - positionTwo[1])
 
 def create_distance_matrix(locations):
-    #Builds a 2D distance matrix used by the OR-tools extension.
+    #Creates a 2D distance matrix used by OR-tools.
+    
     size = len(locations)
     matrix = {}
     for from_idx in range(size):
@@ -28,3 +20,8 @@ def create_distance_matrix(locations):
             )
     return matrix
 
+# Core Functions vvv
+
+def optimize_cart_route(locations, start_index = 0):
+    
+    
